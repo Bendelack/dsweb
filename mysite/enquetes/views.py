11d1d1811a5c715015484038298eaa589fbc3aa6
Bendelack <rickk1010@gmail.com>
2024-03-18 14:39:30 +0000

    commit da primeira tarefa de dsweb

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -4,4 +4,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Aplicação Enquetes - DsWeb 2024.1')
+    return HttpResponse('DsWeb 2024.1 <br> Rick Hill - 20231014040026')
+
+
+def detalhes(request, pergunta_id):
+    resultado = 'DETALHES da enquete de número %s'
+    return HttpResponse(resultado % pergunta_id)
+
+def votacao(request, pergunta_id):
+    resultado = 'VOTACAO da enquete de número %s'
+    return HttpResponse(resultado % pergunta_id)
+
+def resultado(request, pergunta_id):
+    resultado = 'RESULTADO da enquete de número %s'
+    return HttpResponse(resultado % pergunta_id)
