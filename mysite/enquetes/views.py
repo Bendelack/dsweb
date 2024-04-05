00501553c9093a3f4b8823593928bfd9f7355956
Bendelack <rickk1010@gmail.com>
2024-04-05 22:39:59 +0000

    Visualizacao e votacao de enquetes disponiveis

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -1,20 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Pergunta, Alternativa
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('DsWeb 2024.1 <br> Rick Hill - 20231014040026')
-
+    enquetes = Pergunta.objects.order_by('-data_pub')[:10]
+    contexto = { 'lista_enquetes': enquetes }
+    return render(request, 'enquetes/index.html', contexto)
 
 def detalhes(request, pergunta_id):
-    resultado = 'DETALHES da enquete de número %s'
-    return HttpResponse(resultado % pergunta_id)
+    pergunta = get_object_or_404(Pergunta, pk = pergunta_id)
+    contexto = { 'enquete': pergunta }
+    return render(request, 'enquetes/detalhes.html', contexto)
 
 def votacao(request, pergunta_id):
-    resultado = 'VOTACAO da enquete de número %s'
-    return HttpResponse(resultado % pergunta_id)
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+    try:
+        id_alternativa = request.POST['escolha']
+        alt = pergunta.alternativa_set.get(pk=id_alternativa)
+    except (KeyError, Alternativa.DoesNotExist):
+        contexto = { 'enquete': pergunta, 'error': 'Você precisa selecionar uma alternativa.' }
+        return render(request, 'enquetes/detalhes.html', contexto)
+    else:
+        alt.quant_votos += 1
+        alt.save()
+        return HttpResponseRedirect(reverse('enquetes:resultado', args=(pergunta.id,)))
 
 def resultado(request, pergunta_id):
-    resultado = 'RESULTADO da enquete de número %s'
-    return HttpResponse(resultado % pergunta_id)
+    pergunta = get_object_or_404(Pergunta, pk = pergunta_id)
+    contexto = { 'enquete': pergunta }
+    return render(request, 'enquetes/resultado.html', contexto)
