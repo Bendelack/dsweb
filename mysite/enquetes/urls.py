a647a90dbeb9cc35bd85ce40a7295382e4abcfe4
Bendelack <rickk1010@gmail.com>
2024-07-27 19:56:08 +0000

    Classes genericas de visao - ListView e DetailView

--- mysite/enquetes/urls.py
+++ mysite/enquetes/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 
 app_name = 'enquetes'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:pergunta_id>/', views.detalhes, name='detalhes'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetalhesView.as_view(), name='detalhes'),
+    path('<int:pk>/resultado/', views.ResultadoView.as_view(), name='resultado'),
     path('<int:pergunta_id>/votacao/', views.votacao, name='votacao'),
-    path('<int:pergunta_id>/resultado/', views.resultado, name='resultado'),
 ]
--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -2,33 +2,49 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from .models import Pergunta, Alternativa
+from django.views import generic
 
 # Create your views here.
-
-def index(request):
+"""
+def IndexView(generic.ListView):
     enquetes = Pergunta.objects.order_by('-data_pub')[:10]
     contexto = { 'lista_enquetes': enquetes }
     return render(request, 'enquetes/index.html', contexto)
-
-def detalhes(request, pergunta_id):
+"""
+## class IndexView funcionando normalmente
+class IndexView(generic.ListView):
+    template_name = 'enquetes/index.html'
+    context_object_name = 'lista_enquetes'
+    def get_queryset(self):
+        return Pergunta.objects.order_by('-data_pub')[:10]
+"""
+def DetalhesView(request, pergunta_id):
     pergunta = get_object_or_404(Pergunta, pk = pergunta_id)
     contexto = { 'enquete': pergunta }
     return render(request, 'enquetes/detalhes.html', contexto)
+"""
+## class DetalhesView funcionando com erro
+class DetalhesView(generic.DetailView):
+    model = Pergunta
+    template_name = 'enquetes/pergunta_detail.html'
+"""
+class ResultadoView(generic.DetailView):
+    pergunta = get_object_or_404(Pergunta, pk = pergunta_id)
+    return render(request, 'enquetes/resultado.html', { 'enquete': pergunta })
+"""
+class ResultadoView(generic.DetailView):
+    model = Pergunta
+    template_name = 'enquetes/resultado.html'
 
 def votacao(request, pergunta_id):
     pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
     try:
-        id_alternativa = request.POST['escolha']
-        alt = pergunta.alternativa_set.get(pk=id_alternativa)
+        alt = pergunta.alternativa_set.get(pk=request.POST['escolha'])
     except (KeyError, Alternativa.DoesNotExist):
-        contexto = { 'enquete': pergunta, 'error': 'Você precisa selecionar uma alternativa.' }
-        return render(request, 'enquetes/detalhes.html', contexto)
+        return render(request, 'enquetes/pergunta_detail.html', {
+            'pergunta': pergunta, 'error': 'Você precisa selecionar uma alternativa.',
+        })
     else:
         alt.quant_votos += 1
         alt.save()
         return HttpResponseRedirect(reverse('enquetes:resultado', args=(pergunta.id,)))
-
-def resultado(request, pergunta_id):
-    pergunta = get_object_or_404(Pergunta, pk = pergunta_id)
-    contexto = { 'enquete': pergunta }
-    return render(request, 'enquetes/resultado.html', contexto)
